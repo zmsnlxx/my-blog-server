2b56605801e2b05ea986406e3f0e756e39546a8e
liuxiang <liuxiang@aiyunxiao.com>
2019-12-14 20:31:18 +0800

    add: 完成文章标签增删功能

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,7 @@ import Router from 'vue-router';
 
 Vue.use(Router);
 export default new Router({
-    mode: 'history',
+    base: '/server',
     routes: [
         {path: '/', component: () => import('./views/index.vue')},
         {
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/server' : '/',  //根据www.xxx.com/后面的路径写入（比如www.xxx.com/tool）
+  build: {
+    assetsPublicPath: '/server',
+  },
+  // publicPath: process.env.NODE_ENV === 'production' ? '/server' : '/',  //根据www.xxx.com/后面的路径写入（比如www.xxx.com/tool）
   devServer: {
     proxy: {
       "/api": {
