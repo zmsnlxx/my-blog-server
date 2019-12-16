41c72c004cb09f9a01532508acc993eaace2a939
liuxiang <liuxiang@aiyunxiao.com>
2019-12-16 12:01:50 +0800

    add: 完成文章标签增删功能

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   lintOnSave: false,
-  build: {
-    assetsPublicPath: '/server',
-  },
   // publicPath: process.env.NODE_ENV === 'production' ? '/server' : '/',  //根据www.xxx.com/后面的路径写入（比如www.xxx.com/tool）
   devServer: {
     proxy: {
